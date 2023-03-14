926a88e96a8e9163b1b45f6001b70b6ec0c58731
Micheal <mwmakarov@bk.ru>
2023-03-14 17:03:53 +0400

    зона выбора языков в отдельный компонент

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,21 @@
-import { Dispatch, useEffect } from 'react';
+import { Dispatch } from 'react';
 
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 import './App.scss';
 
-import LanguageSelect from '../LanguageSelect/LanguageSelect';
-
-import { loadLanguages, setCurrentInput, translateCurrentWord } from '../../store/actions/appActions';
-import {useDispatch, useSelector} from 'react-redux';
+import { setCurrentInput, translateCurrentWord } from '../../store/actions/appActions';
+import { useDispatch, useSelector } from 'react-redux';
 import { IAppReducer } from '../../store/reducers/appReducer';
 import { store } from '../../store';
+import LanguagesSwitchers from '../LanguagesSwitchers/LanguagesSwitchers';
 
-import arrowRight from './arrow-right.png';
+
 
 const App = () => {
 
     const dispatch = useDispatch();
     const dispatchAsync = store.dispatch as typeof store.dispatch | Dispatch<any>
     const state = useSelector((state: IAppReducer) => state) as IAppReducer;
-
-    useEffect(() => {
-        dispatchAsync(loadLanguages())
-    }, [])
 
     return ( 
         <>
@@ -33,31 +28,8 @@ const App = () => {
             <div className="result">
                 {state.translation}
             </div>
-            <div className="lang-select">
-                {
-                    state.fromLangs?.length && 
-                        <LanguageSelect 
-                            type='from'
-                            placeholder='Choose first language'
-                        />
-                }
-                {
-                    <img src={arrowRight} alt="" className='lang-select__arrow'/>
-                }
-                {
-                    state.toLangs?.length 
-                    ?   <LanguageSelect 
-                            type='to' 
-                            placeholder='Choose second language'
-                        /> 
-                    : state.fromLangs?.length 
-                    ?   <LanguageSelect 
-                            hidden={true}
-                            placeholder='Choose second language'
-                        />
-                    : null
-                }
-            </div>
+
+            <LanguagesSwitchers/>
         </>
     );
 }
---
+++ src/components/LanguagesSwitchers/LanguagesSwitchers.tsx
@@ -0,0 +1,48 @@
+import { Dispatch, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { store } from "../../store";
+import { loadLanguages } from "../../store/actions/appActions";
+import { IAppReducer } from "../../store/reducers/appReducer";
+
+import LanguageSelect from "../LanguageSelect/LanguageSelect";
+
+import arrowRight from './arrow-right.png';
+
+const LanguagesSwitchers = () => {
+    const dispatchAsync = store.dispatch as typeof store.dispatch | Dispatch<any>
+    const state = useSelector((state: IAppReducer) => state) as IAppReducer;
+
+    useEffect(() => {
+        dispatchAsync(loadLanguages())
+    }, [])
+
+    return ( 
+        <div className="lang-select">
+                {
+                    state.fromLangs?.length && 
+                        <LanguageSelect 
+                            type='from'
+                            placeholder='Choose first language'
+                        />
+                }
+                {
+                    <img src={arrowRight} alt="" className='lang-select__arrow'/>
+                }
+                {
+                    state.toLangs?.length 
+                    ?   <LanguageSelect 
+                            type='to' 
+                            placeholder='Choose second language'
+                        /> 
+                    : state.fromLangs?.length 
+                    ?   <LanguageSelect 
+                            hidden={true}
+                            placeholder='Choose second language'
+                        />
+                    : null
+                }
+            </div>
+     );
+}
+ 
+export default LanguagesSwitchers;
