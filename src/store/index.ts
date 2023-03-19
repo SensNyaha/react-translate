ca437f5b55b4db30f2371f4869b2c7e232328f2b
Micheal <mwmakarov@bk.ru>
2023-03-19 22:46:17 +0400

    Updates

--- src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import { appReducer } from "./reducers/appReducer";
-
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from "redux-devtools-extension";
-
-
-export const store = createStore(appReducer, composeWithDevTools(applyMiddleware(thunk)));
